f069e0f959a8c90867c7ffaeff1c04f6e812d7d3
ryalbert <ryanlegrand@hotmail.com>
2020-12-30 21:17:48 +0100

    Minor refactoring

--- animate.py
+++ animate.py
@@ -5,8 +5,9 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from matplotlib import animation, rc
 from IPython.display import HTML
-import main 
+import main
 matplotlib.rcParams['animation.writer'] = 'ffmpeg'
+
 
 Us, t = main.openfile()
 nbodies = int(Us.shape[1]/4)
@@ -18,7 +19,7 @@ fig = plt.figure(figsize=(12,5))
 ax2 = plt.subplot(1,1,1)
 
 # set up the subplots as needed
-bound=np.max(np.abs(Us[:,:nbodies*2])) * 1.10
+bound = np.max(np.abs(Us[:,:nbodies*2])) * 1.10
 ax2.set_xlim(-bound,bound)
 ax2.set_ylim(-bound,bound)
 ax2.set_xlabel('X')
@@ -35,11 +36,11 @@ yi = []
 linei = []
 pti = []
 for i in range(nbodies):
-    color=next(ax2._get_lines.prop_cycler)['color']
+    color = next(ax2._get_lines.prop_cycler)['color']
     linei.append(ax2.plot([],[],    color = color, lw=2) )
     yi.append(Us[:,2*i])
     yi.append(Us[:,2*i+1])
-    pti.append(ax2.plot(  [],[],'.',color = color,ms=20))
+    pti.append(ax2.plot(  [],[],'.',color = color,ms=20) )
 #y1 = Us[:,0]
 
 
@@ -48,23 +49,16 @@ def drawframe(n):
     n = 1 if n < 2 else n
 
     print("Animated " + str(n) +"/"+str(len(t))) if n%100 == 0 else 0
-    #line1.set_data(t, y1)
-    #line2.set_data(t, y2)
     trail_size = 50
     a = n-trail_size if n > trail_size+1 else 0 
+
     for i in range(nbodies):
         linei[i][0].set_data(yi[2*i][a:n], yi[2*i+1][a:n])
         pti[i][0].set_data(yi[2*i][n],yi[2*i+1][n])
-    #line3.set_data(y1[a:n],y2[a:n])    
-    # pt1.set_data(y1[n],y2[n])
-
-   # txt_title.set_text('Frame = {0:4d}'.format(n))
+    
+    # txt_title.set_text('Frame = {0:4d}'.format(n))
     return tuple(linei[i][0] for i in range(nbodies))
 
-
-from matplotlib import animation
-
-# blit=True re-draws only the parts that have changed.
 anim = animation.FuncAnimation(fig, drawframe, frames=len(t), interval=0.1, blit=True)
 #interval, time between frames, frames : number of call to drawframe
 
@@ -72,7 +66,7 @@ anim = animation.FuncAnimation(fig, drawframe, frames=len(t), interval=0.1, blit
 #HTML(anim.to_html5_video())
 # Set up formatting for the movie files
 Writer = animation.writers['ffmpeg']
-writer = Writer(fps=100, metadata=dict(artist='Me'), bitrate=1500)
+writer = Writer(fps=100, metadata=dict(artist='Ryan Albert'), bitrate=1500)
 anim.save('animation.mp4', writer=writer)
 
 
--- main.py
+++ main.py
@@ -1,81 +1,85 @@
 import numpy as np
 from scipy.integrate import odeint
-import matplotlib
+import matplotlib 
 from matplotlib import animation, rc
 from IPython.display import HTML
 matplotlib.rcParams['animation.writer'] = 'ffmpeg'
 
-NBODIES = 7
+NBODIES = 3
 
 
 def openfile():
-	Us = np.loadtxt('outputs/Us.dat')
-	t = np.loadtxt('outputs/t.dat') 
-	return Us, t
+    Us = np.loadtxt('outputs/Us.dat')
+    t = np.loadtxt('outputs/t.dat')
+    return Us, t
 
 
-#The acceleration acting on one body is returned by this function
-def accelG(pos1,pos2,m1,m2):
-    G =1 # 6.67 * 10**(-11)
+def accelG(pos1, pos2, m1, m2):
+    # Returns the acceleration acting on one body
+    G = 1  # 6.67 * 10**(-11)
     d = pos2 - pos1
-    Acceleration= G * m1 * m2 /np.linalg.norm(d)**3* d
+    Acceleration = G * m1 * m2 / np.linalg.norm(d) ** 3 * d
     return(Acceleration)
 
-#Returns all the system masses
-def massesf(nbodies=3, testcase="Stable"):
+def get_Masses(nbodies=3, testcase="Stable"):
+    # Returns all the system masses
     if testcase == "Stable" and nbodies == 3:
-        return([1,1,1])
-    else: 
+        return([1, 1, 1])
+    else:
         return([1]*nbodies)
+
 
 def InitialConditions(nbodies = 3, testcase = "Stable"):
     if testcase == "Stable" and nbodies == 3:
-        #x1=−x2=0.97000436−0.24308753i,x3=0;~V=  ̇x3=−2  ̇x1=−2  ̇x2=−0.93240737−0.86473146i
         init = [0.97000436, -0.24308753,
-               -0.97000436, 0.24308753,
-                         0, 0]
-        initV = [  0.93240737*0.5, 0.86473146*0.5, 
-                0.93240737*0.5, 0.86473146*0.5, 
-                -0.93240737, -0.86473146]
+               -0.97000436,  0.24308753,
+                         0,  0          ]
+        initV = [0.93240737*0.5, 0.86473146*0.5,
+                 0.93240737*0.5, 0.86473146*0.5, 
+                -0.93240737    ,-0.86473146     ]
     else:
-        init  = list(np.random.rand(nbodies*2)*5-2.5 )
-        initV = list(np.random.rand(nbodies*2)*5 - 2.5 )
+        seed = 11
+        init  = list( np.random.RandomState(seed).rand(nbodies*2)  * 5 - 2.5 )
+        initV = list( np.random.RandomState(seed+1).rand(nbodies*2)* 5 - 2.5 )
     U0 =np.array( init + initV )
-    #U0 = [initx,inity, initvx, initvy]
+    # U0 = [initx,inity, initvx, initvy]
     return (U0)
 
-#Differential equation for two bodies
+
 def dudt2D(U,t):
-	#U = X,Y, ... ,Vx,Vy, ...
-    pos = np.array(U[:int(len(U)/2)])
-    nbodies = int(len(pos)/2)
+    # Differential equation for two bodies
+    # U = X,Y, ... ,Vx,Vy, ...
+    Position = np.array(U[:int(len(U)/2)])
+    NBodies = int(len(Position)/2)
 
-    masses = massesf(nbodies = nbodies)
-    accelout = []
-    posdouble = np.split(pos,nbodies)
+    Masses = get_Masses(nbodies=NBodies)
+    Accel_Out = []
+    Position_Pairs = np.split(Position, NBodies)
 
-    for planet,i  in zip(posdouble,range(nbodies)):
-        #could replace with postuplecopy = np.delete(postuple,i) ? index error
-        postuplecopy = list(posdouble)
-        massescopy = list(masses)
+    for Planet, i  in zip(Position_Pairs, range(NBodies)):
+        # could replace with postuplecopy = np.delete(postuple,i) ? index error
+        postuplecopy = list(np.delete(Position_Pairs)
+        massescopy = list(Masses)
         postuplecopy.pop(i)
         massescopy.pop(i)
 
-        for otherplanet,j in zip(postuplecopy,range(nbodies-1)) :
-            accel = accelG(planet, otherplanet, masses[i], massescopy[j])
-            accelout.append(np.array(accel))
+        for OtherPlanet, j in zip(postuplecopy, range(NBodies-1)) :
+            # could divide execution time by two here by computing only once
+            # each acceleration
+            accel = accelG(Planet, OtherPlanet, Masses[i], massescopy[j])
+            Accel_Out.append(np.array(accel))
 
-    summedaccel = [sum(accelout[i:i+nbodies-1]) for i in range(0,len(accelout),nbodies-1)]
-    outputaccel = list(np.array(summedaccel).ravel()) 
+    summedaccel = [sum(Accel_Out[i:i+NBodies-1]) 
+                    for i in range(0,len(Accel_Out),NBodies-1)]
+    outputaccel = list(np.array(summedaccel).ravel() )
 
     Speeds = U[int(len(U)/2):]
-    return(np.concatenate([Speeds, outputaccel] ) )
+    return(np.concatenate([Speeds, outputaccel]) )
 
-
-    
+ 
 #U0 = [initx,inity, initvx, initvy]
 U0 = InitialConditions(nbodies=NBODIES)
-t = np.linspace(0,300,500)
+t = np.linspace(0,10,500)
 
 Us = odeint(dudt2D, U0, t)
 
